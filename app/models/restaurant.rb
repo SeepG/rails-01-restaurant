0820dc110f84aa938b8f08441fd73d091bd3f013
Seep G <seep.torque@gmail.com>
2019-04-17 14:49:09 +1000

    controller, validation,models added

---
+++ app/models/restaurant.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class Restaurant < ApplicationRecord
+	validates :title, presence: true
+	validates :address, presence: true
+	validates :description, presence: true
+	validates :food_type, presence: true
+end
